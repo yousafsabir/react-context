1712162cd4fa77f86301bf39164bc46db70f72ff
yousafsabir <yousafsabir430516@gmail.com>
2022-06-08 18:16:11 +0500

    created product page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar";
+import Products from "./components/Products";
 
 function App() {
     return (
         <div className="App">
             <Navbar />
+            <Products />
         </div>
     );
 }
---
+++ src/components/Products.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { FaTshirt } from "react-icons/fa";
+import { AiFillShopping } from "react-icons/ai";
+
+const Products = () => {
+    const items = [
+        {
+            title: "tshirt",
+            price: "$10.0",
+        },
+        {
+            title: "underwear",
+            price: "$2.5",
+        },
+        {
+            title: "shoes",
+            price: "$30.0",
+        },
+        {
+            title: "sweatshirt",
+            price: "$15.0",
+        },
+        {
+            title: "cap",
+            price: "$5.0",
+        },
+        {
+            title: "socks",
+            price: "$10.0",
+        },
+        {
+            title: "pant",
+            price: "$80.0",
+        },
+        {
+            title: "belt",
+            price: "$10.0",
+        },
+    ];
+
+    return (
+        <div className="Products-Cont my-0 mx-auto max-w-5xl py-8 px-3">
+            <div className="Products-Grid grid grid-cols-4 gap-6">
+                {items.map((item, index) => {
+                    return (
+                        <article
+                            key={index}
+                            className="flex flex-col items-stretch gap-2"
+                        >
+                            <div className="flex h-48 w-full items-center justify-center bg-orange-300">
+                                <FaTshirt className="text-8xl" />
+                            </div>
+                            <div className="flex items-start justify-between ">
+                                <div>
+                                    <h3>{item?.title}</h3>
+                                    <p>{item?.price}</p>
+                                </div>
+                                <AiFillShopping className="cursor-pointer" />
+                            </div>
+                        </article>
+                    );
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default Products;
