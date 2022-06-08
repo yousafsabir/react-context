48bdb7a59e7baddcd59d4a3edab6ac45bdf50a5e
yousafsabir <yousafsabir430516@gmail.com>
2022-06-08 18:22:52 +0500

    created checkout page

---
+++ src/components/Checkout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Checkout = () => {
+    return (
+        <div className="Checkout-Cont my-0 mx-auto max-w-5xl py-8 px-3">
+            <div></div>
+        </div>
+    );
+};
+
+export default Checkout;
